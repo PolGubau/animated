767a68fa9249210ad913f9511396fe293416243d
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-04 22:57:34 +0100

    chore: 🗑️ remove unused path resolution in vite config

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,20 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import path from "node:path";
-import { URL } from "node:url";
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [tailwindcss(), react()],
-	resolve: {
-		// alias: {
-		// 	"@polgubau/animated": path.resolve(
-		// 		__dirname,
-		// 		"../../packages/animations/src",
-		// 	),
-		// },
-	},
 });
